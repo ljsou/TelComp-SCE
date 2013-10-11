9d7b21aa728944822e009c7c22a24402822ef077
ljsou <luisjavier.suarezmeza@gmail.com>
2013-10-11 01:08:18 -0500

    adición de clase para el manejo de sesión

---
+++ src/java/telcomp/retrieval/control/LoginBean.java
@@ -0,0 +1,61 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package telcomp.retrieval.control;
+
+import java.io.IOException;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+import org.primefaces.context.RequestContext;
+
+/**
+ *
+ * @author javier
+ */
+@ManagedBean
+@SessionScoped
+public class LoginBean {
+
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void login(ActionEvent actionEvent) throws IOException {
+        RequestContext context = RequestContext.getCurrentInstance();
+        FacesMessage msg = null;
+        boolean loggedIn = false;
+
+        if (username != null && username.equals("admin") && password != null && password.equals("admin")) {
+            loggedIn = true;
+            msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Welcome", username);
+            FacesContext.getCurrentInstance().getExternalContext().redirect("http://localhost:8084/TelCompTerminal/");
+
+        } else {
+            loggedIn = false;
+            msg = new FacesMessage(FacesMessage.SEVERITY_WARN, "Login Error", "Invalid credentials");
+            FacesContext.getCurrentInstance().getExternalContext().redirect("faces/index.xhtml#tab4");
+        }
+
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+        context.addCallbackParam("loggedIn", loggedIn);
+    }
+}
